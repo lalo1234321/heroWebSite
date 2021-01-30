2c63f5cd85f86df2be9a0685ce918d7953b68b30
lalo1234321 <edominguezc@toluca.tecnm.mx>
2021-01-29 19:02:30 -0600

    adding the login feature

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
+import { AuthContext } from './auth/AuthContext'
+import { authReducer } from './auth/authReducer'
 import { AppRouter } from './routers/AppRouter'
 
+const init = () => {
+    // si no existe entonces retornará un objeto que tendrá la propiedad logged en false
+    return JSON.parse(localStorage.getItem('user')) || {
+        logged : false 
+    };
+}
+
 export const HeroesApp = () => {
+    // el init se ejecuta, luego se pasa al initial state y así se obtiene el estado incial de la aplicación
+    const [user, dispatch] = useReducer(authReducer, {}, init)
+    // ahora el user y el dispatch se podrán usar a lo largo de todo el sistema
+
+    // crear un efecto para cuando el usuario cambié, el localstorage se modifiqué
+    // a la hora de recargar el navegador los valores segirán ahí
+    // si se modifica desde components/hooks, se redibujará todo 
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(user));
+        
+    }, [user])
+
+
     return (
         <div>
-            <AppRouter/>
+            <AuthContext.Provider value={{user, dispatch}}>
+                <AppRouter/>
+            </AuthContext.Provider>     
         </div>
     )
 }
